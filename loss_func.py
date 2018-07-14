76e6e72a8b3745c7b7b366d00300b9072fd349a5
Komal Dhuri <komal.a.dhuri@gmail.com>
2018-07-14 15:34:31 -0700

    Add files via upload
    Cross Entropy Loss and Nce loss

---
+++ loss_func.py
@@ -0,0 +1,104 @@
+import tensorflow as tf
+import numpy as np
+
+def cross_entropy_loss(inputs, true_w):
+    """
+    ==========================================================================
+
+    inputs: The embeddings for context words. Dimension is [batch_size, embedding_size].
+    true_w: The embeddings for predicting words. Dimension of true_w is [batch_size, embedding_size].
+
+    Write the code that calculate A = log(exp({u_o}^T v_c))
+
+    A =
+
+
+    And write the code that calculate B = log(\sum{exp({u_w}^T v_c)})
+
+
+    B =
+
+    ==========================================================================
+    """
+
+
+    true_w_transpose=tf.transpose(true_w) #transpose of embeddings
+    mult_u_o_t_v_c=tf.matmul(true_w_transpose,inputs)
+    softmax_numerator=tf.exp(mult_u_o_t_v_c)
+
+    softmax_denominator=tf.reduce_sum(softmax_numerator,1) #summing over the batchsize
+
+    A=tf.log(softmax_numerator + 1e-10 )
+    B=tf.log(softmax_denominator + 1e-10 )
+
+    
+
+    return tf.subtract(B,A)
+
+def nce_loss(inputs, weights, biases, labels, sample, unigram_prob):
+    """
+    ==========================================================================
+
+    inputs: Embeddings for context words. Dimension is [batch_size, embedding_size].
+    weigths: Weights for nce loss. Dimension is [Vocabulary, embeeding_size].
+    biases: Biases for nce loss. Dimension is [Vocabulary, 1].
+    labels: Word_ids for predicting words. Dimesion is [batch_size, 1].
+    samples: Word_ids for negative samples. Dimension is [num_sampled].
+    unigram_prob: Unigram probability. Dimesion is [Vocabulary].
+
+    Implement Noise Contrastive Estimation Loss Here
+
+    ==========================================================================
+    """
+    u_o=tf.nn.embedding_lookup(weights, labels)
+
+    shape1 = weights.shape
+    embedding_size = shape1[1]
+    shape2 = sample.shape
+    k=shape2[0]
+    u_o = tf.reshape(u_o, [-1, embedding_size])
+
+    q_w=tf.nn.embedding_lookup(weights, sample)
+
+    u_o_transpose=tf.transpose(u_o)
+    q_w_transpose=tf.transpose(q_w)
+    mult_u_o_t_v_c = tf.matmul(inputs, u_o_transpose)
+
+    mult_q_w_t_v_c = tf.matmul(inputs, q_w_transpose)
+
+
+    neg_bias=tf.nn.embedding_lookup(biases,sample)
+    pos_bias=tf.nn.embedding_lookup(biases,labels)
+    neg_bias = tf.reshape(neg_bias, [-1])
+    pos_bias = tf.reshape(pos_bias, [-1])
+    U_score = tf.nn.bias_add(mult_u_o_t_v_c , pos_bias)
+
+    N_score = tf.nn.bias_add(mult_q_w_t_v_c , neg_bias)
+
+
+    unigram_prob_tensor=tf.convert_to_tensor(unigram_prob, dtype=tf.float32)
+
+    P_n = tf.gather(unigram_prob_tensor,sample)
+    P_o = tf.gather(unigram_prob_tensor,labels)
+    P_o = tf.reshape(P_o, [-1])
+
+
+
+
+
+    first_term =tf.subtract(U_score, tf.log(tf.scalar_mul(k,P_o)+1e-10))
+    second_term = tf.subtract(N_score,tf.log(tf.scalar_mul(k,P_n)+1e-10))
+
+
+
+    sigmoid_1 = tf.sigmoid(first_term)
+    sigmoid_2 = tf.sigmoid(second_term)
+
+    A = tf.log(sigmoid_1 + 1e-10)
+    B = tf.reduce_sum(tf.log(1-sigmoid_2 + 1e-10),1)
+
+
+
+
+
+    return tf.add(A,B)
